61d23abd164b59940c750eb922c0ad1cfe5df6d9
EngMohamedRaafat <muhammedraafat@outlook.com>
2022-05-04 01:57:32 +0200

    chore: update production configs

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost:
+    "http://udagram-api-env.eba-wvf3wg22.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
