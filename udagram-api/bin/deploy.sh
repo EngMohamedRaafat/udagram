8d6fd900ef90004775928a0ab27fd81b29a6e362
EngMohamedRaafat <muhammedraafat@outlook.com>
2022-05-05 09:48:37 +0200

    chore: Update circleci pipeline configs

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 cd ./www
-eb init $EB_APP --region $AWS_REGION --platform node.js
+eb init $EB_APP --region $AWS_DEFAULT_REGION --platform node.js
 eb use $EB_ENV
 eb setenv POSTGRES_HOST=$POSTGRES_HOST PORT=$PORT POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB
 eb deploy
